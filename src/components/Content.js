8f6dcae1d07198c88215f9f8d68234c9cb20e41e
Sivakumar <119665983+sivasreact@users.noreply.github.com>
2023-12-18 22:04:50 +0530

    Update Content.js

--- src/components/Content.js
+++ src/components/Content.js
@@ -6,7 +6,6 @@ const Content = ({items, handleCheck, handleDelete,newItem,setNewItem,handleSubm
     <div className='content'>
             <section className="section-items">
               <Additem newItem={newItem} setNewItem={setNewItem} handleSubmit={handleSubmit}/>
-              <Search search={search} setSearch={setSearch} />
               {(items.length >0) ? (
                   <ul>
                       {items.map((item) => (
